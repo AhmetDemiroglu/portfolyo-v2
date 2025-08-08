c759f9666e5487b6098184ae17a2497da2691840
Ahmet Demiroğlu <ahmetdemiroglu89@hotmail.com>
2025-08-08 09:48:35 +0300

    .

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,28 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute, useRouter } from '@tanstack/react-router'
 import { Header } from '../components/Header'
 import { ThemeProvider } from '../contexts/ThemeContext'
+import { useEffect } from 'react'
 
+function GitHubPagesRedirectHandler() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get('p');
+
+    if (redirectPath) {
+      window.history.replaceState({}, '', window.location.pathname);
+      router.navigate({ to: redirectPath, replace: true });
+    }
+  }, [router]); 
+
+  return null;
+}
 
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider>
+      <GitHubPagesRedirectHandler />
       <Header />
       <main className="min-h-screen bg-white text-slate-800 dark:bg-slate-900 dark:text-white transition-colors duration-300 ">
         <Outlet />
